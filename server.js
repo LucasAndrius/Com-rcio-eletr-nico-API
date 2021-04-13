1c900314fd8f2747b80410bc2096fb18f2147e75
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-13 14:41:20 -0300

    concluindo config base

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 
@@ -22,3 +22,13 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({extended:true}));
 server.use(fileupload());
+
+server.use(express.static(__dirname+'/public'));
+
+server.get('/ping',(req, res)=>{
+    res.json({pong:true});
+});
+
+server.listen(process.env.PORT,()=>{
+    console.log(`- Rodando no endereço: ${process.env.BASE}`)
+});
