f95445c9214b46f6ea5129ad83c58d2d12a214c8
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-13 14:27:37 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error',(error)=>{
+    console.log("Erro: ",error.message);
+});
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+server.use(fileupload());
