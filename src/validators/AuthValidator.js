e38d841f299859357789438d408bc723e90c391d
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-14 20:16:22 -0300

    finalizando signup

---
+++ src/validators/AuthValidator.js
@@ -0,0 +1,28 @@
+const { checkSchema } = require('express-validator');
+
+module.exports = {
+    signup: checkSchema({
+        name: {
+            trim:true,
+            isLength:{
+                options:{ min: 2 }
+            },
+            errorMessage: 'Nome precisa pelo menos dois caracteres!'
+        },
+        email: {
+            isEmail: true,
+            normalizeEmail:true,
+            errorMessage:'E-mail inválido'
+        },
+        password: {
+            isLength: {
+                options: {min:2}
+            },
+            errorMessage:'Senha precisa no mínimo dois caracteres!'
+        },
+        state: {
+            notEmpty:true,
+            errorMessage: 'Estado não preenchido!'
+        }
+    })
+};
