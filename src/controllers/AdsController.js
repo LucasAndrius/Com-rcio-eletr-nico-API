95e3d63f7443b6e5515d5b84669bd97c90bb8e97
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-14 23:37:00 -0300

    pegando categorias

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,9 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { validationResult, matchedData } = require('express-validator');
+
+const Category = require('../models/Category');
+
 module.exports = {
     getCategories: async (req,res) =>{
+        const cats = await Category.find();
 
+        let categories = [];
+
+        for(let i in cats){
+            categories.push({
+                ...cats[i]._doc, 
+                img:`${process.env.BASE}/assets/images/${cats[i].slug}.png`
+            });
+        }
+
+        res.json({categories});
     },
     addAction: async (req,res) =>{
-
+        
     },
     getList: async (req,res) =>{
 
